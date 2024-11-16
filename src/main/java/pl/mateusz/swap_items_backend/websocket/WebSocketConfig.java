0a15ca6a45965773d5064d14da069e4441063169
mateusz <mat3uszkulak@gmail.com>
2024-11-16 19:09:08 +0100

    Refactor websocket classes

--- src/main/java/pl/mateusz/swap_items_backend/websocket/WebSocketConfig.java
+++ src/main/java/pl/mateusz/swap_items_backend/websocket/WebSocketConfig.java
@@ -2,7 +2,6 @@ package pl.mateusz.swap_items_backend.websocket;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -12,8 +11,6 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 @RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
-//    private final JwtChannelInterceptor jwtChannelInterceptor;
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -25,16 +22,4 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/gs-guide-websocket");
     }
-
-//    @Override
-//    public void registerStompEndpoints(StompEndpointRegistry registry) {
-//        registry.addEndpoint("/gs-guide-websocket")
-//                .addInterceptors(new AuthHandshakeInterceptor())  // Custom handshake interceptor
-//                .setAllowedOrigins("*");
-//    }
-//
-//    @Override
-//    public void configureClientInboundChannel(ChannelRegistration registration) {
-//        registration.interceptors(jwtChannelInterceptor);
-//    }
 }
